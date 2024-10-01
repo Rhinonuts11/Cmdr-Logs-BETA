cc42b310c08589d9cc6e53dcce14b4e828457d0f
Rhinonuts11 <161979906+Rhinonuts11@users.noreply.github.com>
2024-10-01 18:32:49 +0100

    Update check-access.js

--- CLS/public/js/check-access.js
+++ CLS/public/js/check-access.js
@@ -89,7 +89,7 @@ function setCookie(name, value, days) {
 // The handler for the click on the authorization button
 document.getElementById('auth-button').addEventListener('click', () => {
     const clientId = '1288151544159539274';
-    const redirectUri = 'YOUR_SITE_URL/.netlify/functions/callback';
+    const redirectUri = 'https://testban.netlify.app/.netlify/functions/callback';
     const scope = 'identify email';
     const authUrl = `https://discord.com/api/oauth2/authorize?client_id=${clientId}&redirect_uri=${encodeURIComponent(redirectUri)}&response_type=code&scope=${encodeURIComponent(scope)}`;
     window.location.href = authUrl;
